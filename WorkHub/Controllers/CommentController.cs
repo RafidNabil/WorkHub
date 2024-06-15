5bed3afae6c2f5dd7b580436255cca64841f87b6
RafidNabil <rafidnabil3@gmail.com>
2024-06-15 21:10:58 +0600

    Initial Commit

---
+++ WorkHub/Controllers/CommentController.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkHub.Models;
+using WorkHub.Services;
+
+namespace WorkHub.Controllers
+{
+    public class CommentController : Controller
+    {
+        private IMongoCollection<Comments> _commentCollection;
+
+        public CommentController()
+        {
+            _commentCollection = MongoDBHelper.GetCollection<Comments>("comments");
+        }
+
+        public ActionResult Index()
+        {
+            var comments = _commentCollection.Find(FilterDefinition<Comments>.Empty).Limit(10).ToList();
+            return View(comments);
+        }
+    }
+}
