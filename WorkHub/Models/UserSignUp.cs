e3244c845f0539e39eacc1746768de16202d41a8
RafidNabil <rafidnabil3@gmail.com>
2024-06-23 00:21:47 +0600

    Manger UI, Login - Signup, Role modifying

---
+++ WorkHub/Models/UserSignUp.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls;
+
+namespace WorkHub.Models
+{
+    public class UserSignUp
+    {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [Required(ErrorMessage = "Username is required")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; }
+
+    public string Role { get; set; } = "developer";
+
+    }
+}
