a5829a128f5011c9c022f392f606db5247bbb9dd
RafidNabil <rafidnabil3@gmail.com>
2024-06-23 01:55:56 +0600

    UI

---
+++ WorkHub/Controllers/Manager/ManagerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkHub.Models;
+
+namespace WorkHub.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult ManagerIndex()
+        {
+            return View();
+        }
+
+        // GET: Manager/CreateProject
+        public ActionResult CreateProject()
+        {
+            return View();
+        }
+
+        // POST: Manager/CreateProject
+        [HttpPost]
+        public ActionResult CreateProject(CreateaProject model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the project to the database (code to save the project goes here)
+                return RedirectToAction("ManagerIndex");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult ManagerDashboard()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WorkHub/Models/CreateaProject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WorkHub.Models
+{
+    public class CreateaProject
+    {
+        [Required]
+        public string ProjectName { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public List<int> TeamMembers { get; set; }
+    }
+}
